16621c3dca57400d8ce5b42714c174891b89b40e
bnmahajan78 <bnmahajan78@gmail.com>
2022-07-31 14:26:09 +0530

    Update deployansible.sh

--- src/deployansible.sh
+++ src/deployansible.sh
@@ -10,8 +10,6 @@ mip=''
 masterip=''
 masterip1=''
 cmdd=""
-
-
 masterip=$(curl ifconfig.me  )
 masterip1=$(curl ifconfig.me  )
 cmdd="'sudo echo  $masterip1 Master.example.com  | sudo -S tee -a /etc/hosts'"
@@ -21,8 +19,8 @@ echo $mfile
 
 while read -r line; do
 			echo   -e "$line\n"
-			echo   "=============7.1 Master :  ================"
-            mip=$line   
+			echo   "=============7.1 Master :Make backup of authorized_keys ================"
+            mip=$line
 		  
 done <$mfile		  
 echo $mip
@@ -35,15 +33,12 @@ echo $mip
 		 
 ##ssh -i ~/.ssh/custkey  -o  StrictHostKeyChecking=no  -o ControlMaster=no  ubuntu@$mip 'rm -r ~/myscripts' <<EOF
 ssh -i ~/.ssh/custkey  -o  StrictHostKeyChecking=no  -o ControlMaster=no  ubuntu@$mip 'mkdir ~/myscripts' </dev/null
-
 scp -i ~/.ssh/custkey   ~/myscripts/masternode ubuntu@$mip:~/myscripts/masternode
 scp -i ~/.ssh/custkey   ~/myscripts/iplist ubuntu@$mip:~/myscripts/iplist
-
 scp -i ~/.ssh/custkey   ~/myscripts/custkey ubuntu@$mip:~/myscripts/custkey
 scp -i ~/.ssh/custkey   ~/myscripts/custkey.pub ubuntu@$mip:~/myscripts/custkey.pub
 scp -i ~/.ssh/custkey   ~/myscripts/ansiconf ubuntu@$mip:~/myscripts/ansiconf
-
-scp -i ~/.ssh/custkey   ~/myscripts/sub1.sh ubuntu@$mip:~/myscripts/sub1.sh
+ scp -i ~/.ssh/custkey   ~/myscripts/sub1.sh ubuntu@$mip:~/myscripts/sub1.sh
  
    wfile='/home/ubuntu/myscripts/iplist'
  
@@ -63,8 +58,8 @@ scp -i ~/.ssh/custkey   ~/myscripts/sub1.sh ubuntu@$mip:~/myscripts/sub1.sh
 		
          done <$wfile
 		 
-	 
-		 cmdd2="/bin/bash  ~/myscripts/sub1.sh  $mip"
+		
+		   cmdd2="/bin/bash  ~/myscripts/sub1.sh  $mip"
          ssh -i ~/.ssh/custkey  -o  StrictHostKeyChecking=no  -o ControlMaster=no  ubuntu@$mip "${cmdd2}" </dev/null
           
          
